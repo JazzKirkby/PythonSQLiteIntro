7961d19b9e478d0efd255c2d3efc97a4af6a4852
JazzK <jhk73@bath.ac.uk>
2025-04-10 09:44:59 +0000

    Create DB, Drop Table, Create Table

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-#imports sqlite3
 import sqlite3
-#creates the store DB
 conn = sqlite3.connect('store')
-#creates the table called pet
-conn.execute("CREATE TABLE 'pet' (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE)")
+print ("Database has been created")
+
+conn.execute("DROP TABLE IF EXISTS pet")
+
+conn.execute("CREATE TABLE pet (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE)")
+
+print ("Table created successfully")
