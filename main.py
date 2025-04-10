c78beabc966fc322b706d24e88d62196ccc5aa84
JazzK <jhk73@bath.ac.uk>
2025-04-10 09:39:28 +0000

    Adding comments to explain the code

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#imports sqlite3
 import sqlite3
+#creates the store DB
 conn = sqlite3.connect('store')
+#creates the table called pet
 conn.execute("CREATE TABLE 'pet' (name VARCHAR(20), owner VARCHAR(20), species VARCHAR(20), sex CHAR(1), checkups SMALLINT UNSIGNED, birth DATE, death DATE)")
